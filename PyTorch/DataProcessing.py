5e56af81ed6373934c59c05e570416a1a5115ad9
ad039 <alexdunn2001@gmail.com>
2024-07-20 18:13:31 +1000

    Changed FIle location of Data

--- PyTorch/DataProcessing.py
+++ PyTorch/DataProcessing.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-xy = pd.read_csv('./PyTorch/Alex_Motion_Cap_1_edited.csv', skiprows=2, skip_blank_lines=0,na_values=[''])
-xy = xy.dropna()
-xy.drop("Frame", axis='columns')
+MotionCaptureData = pd.read_csv('./PyTorch/Alex_Motion_Cap_1_edited.csv', skiprows=2, skip_blank_lines=0,na_values=[''])
+BLESensorData = pd.read_csv('./PyTorch/output_test_motion_cap.csv')
 
-xy.to_csv('./PyTorch/Alex_Motion_Cap_Formatted.csv')
+
+#.to_csv('./PyTorch/Alex_Motion_Cap_Formatted.csv')
 
 
 
