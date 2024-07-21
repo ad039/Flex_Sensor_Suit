141e2c496d118a6c4a7c52959323b1267eb6d2f1
ad039 <alexdunn2001@gmail.com>
2024-07-21 12:01:16 +1000

    changes to DataProcessing

--- PyTorch/DataProcessing.py
+++ PyTorch/DataProcessing.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 MotionCaptureData = pd.read_csv('./PyTorch/Alex_Motion_Cap_1_edited.csv', skiprows=2, skip_blank_lines=0,na_values=[''])
 BLESensorData = pd.read_csv('./PyTorch/output_test_motion_cap.csv')
 
+plt.plot(MotionCaptureData.loc[:,"Arm:Hand TopY"])
+plt.plot(BLESensorData.ShoulderFlex1)
+plt.legend(["Motion Cap", "ShoulderFlex2"])
+plt.show()
 
 #.to_csv('./PyTorch/Alex_Motion_Cap_Formatted.csv')
 
