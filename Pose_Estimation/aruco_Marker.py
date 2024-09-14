6b6db3ecfb2b93dcc48ff1d6516c0ba3a7778607
ad039 <alexdunn2001@gmail.com>
2024-09-14 11:37:29 +1000

    update

--- Pose_Estimation/aruco_Marker.py
+++ Pose_Estimation/aruco_Marker.py
@@ -51,7 +51,6 @@ def euler_from_quaternion(x, y, z, w):
       
   return roll_x, pitch_y, yaw_z # in radians
 
-
 def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
     '''
     This will estimate the rvec and tvec for each of the marker corners detected by:
@@ -76,41 +75,25 @@ def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
         tvecs.append(t)
         trash.append(nada)
     return np.array([rvecs]), np.array([tvecs]), trash
- 
-def main():
-  """
-  Main method of the program.
-  """
- 
-  # Load the camera parameters from the saved file
-  cv_file = cv2.FileStorage(
-    camera_calibration_parameters_filename_logi, cv2.FILE_STORAGE_READ) 
-  mtx = cv_file.getNode('K').mat()
-  dst = cv_file.getNode('D').mat()
-  cv_file.release()
 
-  marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
-                        [marker_size / 2, marker_size / 2, 0],
-                        [marker_size / 2, -marker_size / 2, 0],
-                        [-marker_size / 2, -marker_size / 2, 0]], dtype=np.float32)
-   
-  # Start the video stream
-  cap = cv2.VideoCapture(0)
-   
-  while(True):
-  
-    # Capture frame-by-frame
-    # This method returns True/False as well
-    # as the video frame.
-    ret, frame = cap.read()  
-     
+def aruco_pose_estimation(frame, matrix_coefficients, distortion_coefficients):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow("gray", gray)
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_7X7_250) # cv2.aruco.DICT_4X4_250 seleccion del modelo Aruco
+    parameters =  cv2.aruco.DetectorParameters()
+    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+    corners, ids, rejected_img_points = detector.detectMarkers(gray)
+
+    vecs = []
+    xyz = []
+    rpy = []
+        
     # Detect ArUco markers in the video frame
     (corners, marker_ids, rejected) = detector.detectMarkers(
       frame)
        
     # Check that at least one ArUco marker was detected
     if marker_ids is not None:
-      print("detected")
  
       # Draw a square around detected markers in the video frame
       cv2.aruco.drawDetectedMarkers(frame, corners, marker_ids)
@@ -119,8 +102,8 @@ def main():
       rvecs, tvecs, obj_points = my_estimatePoseSingleMarkers(
         corners,
         marker_size,
-        mtx,
-        dst)
+        matrix_coefficients,
+        distortion_coefficients)
       
       # Print the pose for the ArUco marker
       # The pose of the marker is with respect to the camera lens frame.
@@ -132,9 +115,10 @@ def main():
       for i, marker_id in enumerate(marker_ids):
        
         # Store the translation (i.e. position) information
-        transform_translation_x = tvecs[i][0][0]
-        transform_translation_y = tvecs[i][0][1]
-        transform_translation_z = tvecs[i][0][2]
+        vecs.append(np.squeeze(tvecs[i][0][0]))     # x translation
+        vecs.append(np.squeeze(tvecs[i][0][1]))     # y translation
+        vecs.append(np.squeeze(tvecs[i][0][2]))     # z translation
+
  
         # Store the rotation information
         rotation_matrix = np.eye(4)
@@ -153,31 +137,87 @@ def main():
                                                        transform_rotation_y, 
                                                        transform_rotation_z, 
                                                        transform_rotation_w)
-         
-        roll_x = math.degrees(roll_x)
-        pitch_y = math.degrees(pitch_y)
-        yaw_z = math.degrees(yaw_z)
-        print("transform_translation_x: {}".format(transform_translation_x))
-        print("transform_translation_y: {}".format(transform_translation_y))
-        print("transform_translation_z: {}".format(transform_translation_z))
-        print("roll_x: {}".format(roll_x))
-        print("pitch_y: {}".format(pitch_y))
-        print("yaw_z: {}".format(yaw_z))
-        print()
+        
+        rotation_matrix = cv2.Rodrigues(np.array(rvecs[i][0]))[0]  #
+        R_T = rotation_matrix.T
+        T = tvecs[0].T
+
+        xyz = np.squeeze(np.dot(R_T, - T))
+        
+        rpy = np.deg2rad(cv2.RQDecomp3x3(R_T)[0])
+
+        vecs.append(math.degrees(roll_x))
+        vecs.append(math.degrees(pitch_y))
+        vecs.append(math.degrees(yaw_z))
          
         # Draw the axes on the marker
-        cv2.drawFrameAxes(frame, mtx, dst, rvecs[i], tvecs[i], 0.05)
-       
+        cv2.drawFrameAxes(frame, matrix_coefficients, distortion_coefficients, rvecs[i], tvecs[i], 0.05)
+    return frame, xyz, rpy, np.array(vecs)
+ 
+def main():
+  """
+  Main method of the program.
+  """
+ 
+  # Load the camera parameters from the saved file
+  cv_file_logi = cv2.FileStorage(
+    camera_calibration_parameters_filename_logi, cv2.FILE_STORAGE_READ) 
+  mtx_logi = cv_file_logi.getNode('K').mat()
+  dst_logi = cv_file_logi.getNode('D').mat()
+  cv_file_logi.release()
+
+  cv_file_webc = cv2.FileStorage(
+  camera_calibration_parameters_filename_webc, cv2.FILE_STORAGE_READ) 
+  mtx_webc = cv_file_webc.getNode('K').mat()
+  dst_webc = cv_file_webc.getNode('D').mat()
+  cv_file_webc.release()
+
+  marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
+                        [marker_size / 2, marker_size / 2, 0],
+                        [marker_size / 2, -marker_size / 2, 0],
+                        [-marker_size / 2, -marker_size / 2, 0]], dtype=np.float32)
+   
+  # Start the video stream
+  cap1 = cv2.VideoCapture(0)
+  cap2 = cv2.VideoCapture(2)
+   
+  while (cap1.isOpened() and cap2.isOpened()):
+  
+    # Capture frame-by-frame
+    # This method returns True/False as well
+    # as the video frame.
+    ret, frame_logi = cap1.read() 
+    ret, frame_webc = cap2.read()  
+
+    frame_logi, xyz_logi, rpy_logi, vecs_logi = aruco_pose_estimation(frame_logi, mtx_logi, dst_logi)
+    frame_webc, xyz_webc, rpy_webc, vecs_webc = aruco_pose_estimation(frame_webc, mtx_webc, dst_webc)
+
     # Display the resulting frame
-    cv2.imshow('frame',frame)
-          
+    cv2.imshow('frame logi',frame_logi)
+    cv2.imshow('frame webc',frame_webc)
+    
+    #print(vecs_logi, vecs_webc)  
+    print(vecs_logi)
+    '''
+    theta_logi = np.deg2rad(-vecs_logi[3]+180)
+    rotation_matrix_logi = np.array([[np.cos(theta_logi), 0, np.sin(theta_logi)],
+                                     [0, 1, 0],
+                                     [-np.sin(theta_logi), 0, np.cos(theta_logi)]])
+    print((vecs_logi[0:3].reshape(3, -1)).shape)
+    
+    camera_logi_vec = np.matmul(rotation_matrix_logi, (vecs_logi[0:3].reshape(3, -1)))
+    '''
+    print(vecs_logi)
+    print(xyz_logi, rpy_logi)
+
     # If "q" is pressed on the keyboard, 
     # exit this loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
   
   # Close down the video stream
-  cap.release()
+  cap1.release()
+  cap2.release()
   cv2.destroyAllWindows()
    
 if __name__ == '__main__':
